5062af0be656a76c980e42a5acfca2505b3aa0e0
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-08-18 21:57:35 -0300

    jwt verify

--- middlewares/validateJwt.js
+++ middlewares/validateJwt.js
@@ -1,16 +1,36 @@
-const jwt = require('')
+const jwt = require('jsonwebtoken')
 
 
 
-const validateJWT = (req,res,next) =>{
+const validateJWT = (req,res,next) => {
 
     //x-token headers
 
     const token = req.header('x-token')
 
-    
+    if(!token){
 
-    console.log(token)
+        return res.status(401).json({
+            ok:false,
+            msg:'no token in the header'
+        })
+        try {
+            
+            const payload =jwt.verify(token,process.env.SECRET_JWT_SEED)
+
+        } catch (error) {
+
+            return res.status(401).json({
+                ok:false,
+                msg:'token invalid'
+            })
+        }
+
+
+    }
+
+
+   
 
     next()
 }
